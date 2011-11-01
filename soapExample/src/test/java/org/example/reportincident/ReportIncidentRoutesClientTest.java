1546c482bdc18c72d7396c77755e4555acde248b
cortmann <christoph.ortmann@opitz-consulting.com>
2011-11-01 14:03:40 +0100

    added feature for deploying on karaf

--- soapExample/src/test/java/org/example/reportincident/ReportIncidentRoutesClientTest.java
+++ soapExample/src/test/java/org/example/reportincident/ReportIncidentRoutesClientTest.java
@@ -14,53 +14,55 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class ReportIncidentRoutesClientTest extends CamelSpringTestSupport {
 
-    // should be the same address as we have in our route
-    private static final String URL = "http://localhost:8182/cxf/camel-example-cxf-osgi/webservices/incident";
-   
-    protected static ReportIncidentEndpoint createCXFClient() {
-        // we use CXF to create a client for us as its easier than JAXWS and works
-        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-        factory.setServiceClass(ReportIncidentEndpoint.class);
-        factory.setAddress(URL);
-        return (ReportIncidentEndpoint) factory.create();
-    }
+	// should be the same address as we have in our route
+	private static final String URL = "http://localhost:8181/cxf/camel-example-cxf-osgi/webservices/incident";
 
-    @Test
-    public void testRendportIncident() throws Exception {
-        runTest();
-    }
-    
-    protected void runTest() throws Exception {
-        // create input parameter
-        InputReportIncident input = new InputReportIncident();
-        input.setIncidentId("123");
-        input.setIncidentDate("2008-08-18");
-        input.setGivenName("Claus");
-        input.setFamilyName("Ibsen");
-        input.setSummary("Bla");
-        input.setDetails("Bla bla");
-        input.setEmail("davsclaus@apache.org");
-        input.setPhone("0045 2962 7576");
+	protected static ReportIncidentEndpoint createCXFClient() {
+		// we use CXF to create a client for us as its easier than JAXWS and
+		// works
+		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+		factory.setServiceClass(ReportIncidentEndpoint.class);
+		factory.setAddress(URL);
+		return (ReportIncidentEndpoint) factory.create();
+	}
 
-        // create the webservice client and send the request
-        ReportIncidentEndpoint client = createCXFClient();
-        log.info("calling with name:" + input.getGivenName());
-        OutputReportIncident out = client.reportIncident(input);
+	@Test
+	public void testRendportIncident() throws Exception {
+		runTest();
+	}
 
-        // assert we got a OK back
-        assertEquals("ok", out.getCode());
-        
-        // test the input with other users
-        input.setGivenName("Guest");
-        log.info("calling with name:" + input.getGivenName());
-        out = client.reportIncident(input);
-        // assert we got a Accept back
-        assertEquals("accepted", out.getCode());
-        //while (true){Thread.sleep(1000);}
-    }
+	protected void runTest() throws Exception {
+		// create input parameter
+		InputReportIncident input = new InputReportIncident();
+		input.setIncidentId("123");
+		input.setIncidentDate("2008-08-18");
+		input.setGivenName("Claus");
+		input.setFamilyName("Ibsen");
+		input.setSummary("Bla");
+		input.setDetails("Bla bla");
+		input.setEmail("davsclaus@apache.org");
+		input.setPhone("0045 2962 7576");
 
-    @Override
-    protected AbstractApplicationContext createApplicationContext() {
-        return new ClassPathXmlApplicationContext(new String[] {"camel-context.xml"});
-    }
+		// create the webservice client and send the request
+		ReportIncidentEndpoint client = createCXFClient();
+		log.info("calling with name:" + input.getGivenName());
+		OutputReportIncident out = client.reportIncident(input);
+
+		// assert we got a OK back
+		assertEquals("ok", out.getCode());
+
+		// test the input with other users
+		input.setGivenName("Guest");
+		log.info("calling with name:" + input.getGivenName());
+		out = client.reportIncident(input);
+		// assert we got a Accept back
+		assertEquals("accepted", out.getCode());
+		// while (true){Thread.sleep(1000);}
+	}
+
+	@Override
+	protected AbstractApplicationContext createApplicationContext() {
+		return new ClassPathXmlApplicationContext(
+				new String[] { "camel-context.xml" });
+	}
 }
